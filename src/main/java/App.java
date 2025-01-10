451dd058e4a7a69feda0877e5bccc0605f8a771f
lavish-jhamb <lavishjhamb1@gmail.com>
2025-01-10 19:30:14 +0530

    Add Sysout Statment

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,6 @@ public class App
     {
         System.out.println("Jenkins crash course");
         System.out.println("CI/CD");
+        System.out.println("Great! You have run your first pipeline");
     }
 }
