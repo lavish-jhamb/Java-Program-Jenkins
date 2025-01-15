a6a119c78e87fa1b63644733e7ed22b2f4cd5d8e
lavish-jhamb <lavishjhamb1@gmail.com>
2025-01-15 12:24:29 +0530

    add print statement

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,5 +9,6 @@ public class App
         System.out.println("Jenkins crash course");
         System.out.println("CI/CD");
         System.out.println("Great! You have run your first pipeline");
+        System.out.prin("Success!");
     }
 }
