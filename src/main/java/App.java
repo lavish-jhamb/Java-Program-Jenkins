8e0467e2acbacf66cc5b7df93e84b790fcb5b678
lavish-jhamb <lavishjhamb1@gmail.com>
2025-01-15 12:26:27 +0530

    add error

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,9 +6,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Jenkins crash course");
-        System.out.println("CI/CD");
-        System.out.println("Great! You have run your first pipeline");
-        System.out.prin("Success!");
+        int a = 50/0;
     }
 }
